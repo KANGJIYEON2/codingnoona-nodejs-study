b3d8faf2831760637d4546f9c34b229b99834396
KANGJIYEON2 <hihello516@naver.com>
2024-06-01 14:26:19 +0900

    update

--- shopping-app/fe/src/page/Login.js
+++ shopping-app/fe/src/page/Login.js
@@ -3,20 +3,20 @@ import { Container, Form, Button, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../action/userAction";
-
+import { Circles } from "react-loader-spinner";
 import "../style/login.style.css";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { user } = useSelector((state) => state.user);
+  const { loading, user } = useSelector((state) => state.user);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const error = useSelector((state) => state.user.error);
 
   const loginWithEmail = (event) => {
     event.preventDefault();
-    //이메일,패스워드를 가지고 백엔드로 보내기
+    dispatch(userActions.loginWithEmail({ email, password }));
   };
 
   const handleGoogleLogin = async (googleData) => {
@@ -28,6 +28,12 @@ const Login = () => {
   }
   return (
     <>
+      {loading && (
+        <div>
+          <Circles />
+        </div>
+      )}
+
       <Container className="login-area">
         {error && (
           <div className="error-message">
