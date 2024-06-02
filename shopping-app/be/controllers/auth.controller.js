70adc677efe1dce631564a0d5ed0da19e1c007ef
KANGJIYEON2 <hihello516@naver.com>
2024-06-02 23:09:15 +0900

    update

--- shopping-app/be/controllers/auth.controller.js
+++ shopping-app/be/controllers/auth.controller.js
@@ -1,5 +1,10 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const { request } = require("express");
+require("dotenv").config;
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 const authController = {};
 
 authController.loginWithEmail = async (req, res) => {
@@ -14,6 +19,21 @@ authController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error("invalid email or password");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) throw new Error("token not found");
+    const token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error("invalid token");
+      req.userId = payload._id;
+    });
+    next();
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
